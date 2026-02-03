e20b63115a21d7706fe2c0111f503fe0ec98adb4
MiriamJimenesIesBelen <mjimsan1710@g.educaand.es>
2026-02-03 10:54:02 +0100

    cambios

--- mobiles/src/app/product-details/product-details.spec.ts
+++ mobiles/src/app/product-details/product-details.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
-import { ProductDetailsComponent } from './product-details'; // Asegúrate de que el archivo se llame product-details.ts
+import { ProductDetailsComponent } from './product-details';
 
 describe('ProductDetailsComponent', () => {
   let component: ProductDetailsComponent;
@@ -8,17 +8,17 @@ describe('ProductDetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      // En Angular 17/18/21, los componentes standalone van en 'imports'
+
       imports: [ProductDetailsComponent],
       providers: [
-        provideRouter([]) // Necesario porque el tutorial usa rutas para los detalles
+        provideRouter([])
       ]
     })
       .compileComponents();
 
     fixture = TestBed.createComponent(ProductDetailsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges(); // Ejecuta la detección de cambios inicial
+    fixture.detectChanges();
   });
 
   it('should create', () => {
